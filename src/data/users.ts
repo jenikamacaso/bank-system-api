a86c9c168d901bf72dd9c467556a790a9b53abc6
jenikamacaso <jenikamacaso@gmail.com>
2026-02-11 15:40:55 +0800

    ** removed unnecessary code

--- src/data/users.ts
+++
@@ -1,9 +0,0 @@
-export interface User {
-  username: string;
-  password: string;
-}
-
-export const users: User[] = [
-  { username: 'admin', password: 'password123' },
-  { username: 'staff', password: 'password456' },
-];
